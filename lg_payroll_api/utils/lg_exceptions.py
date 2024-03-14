90e9e07f823a6fcb2a45aca99dab214e6155e76b
Diogo56 <diogo.amorim2001@gmail.com>
2024-03-13 22:41:00 -0300

    chore: documentation

--- lg_payroll_api/utils/lg_exceptions.py
+++ lg_payroll_api/utils/lg_exceptions.py
@@ -1,13 +1,3 @@
-from enum import Enum
-
-
-class LGStatus(Enum):
-    SUCCESS = 0
-    INCONSISTENCE = 1
-    ERROR = 2
-    NOT_PROCESS = 3
-
-
 class LgException(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
