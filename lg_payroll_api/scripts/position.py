f626e402b6a9a239982bec601b1954311c91cfc0
Diogo56 <diogo.amorim2001@gmail.com>
2025-02-17 23:46:51 -0300

    chore: add positions service documentation

--- lg_payroll_api/scripts/position.py
+++ lg_payroll_api/scripts/position.py
@@ -44,14 +44,27 @@ class LgApiPositionClient(BaseLgServiceClient):
         records_limit: int = 50,
         permission_to_recorded_employee: bool = None,
         only_actives: bool = None,
-        consider_freeze: bool = None,
+        consider_freezed: bool = None,
         only_officials: bool = None,
         not_temporaries: bool = None,
         search_term: str = None
-    ) -> LgApiReturn:  
+    ) -> LgApiReturn:
+        """Consult list of positions.
+
+        Args:
+            company_code (int, mandatory): The company code
+            records_limit (int, optional): Quantity of records to return in requisition, 50 by default
+            permission_to_recorded_employee (bool, optional): Flag to return only positions with permition
+            to recorded employees
+            only_actives (bool, optional): Flag to return only active positions
+            consider_freezed (bool, optional): Flag to consider freezed positions
+            only_officials (bool, optional): Flag to return only official positions
+            not_temporaries (bool, optional): Flag to not return temporaries positions
+            search_term (str, optional): Search term
+        """
         permission_to_recorded_employee = self.__bool_to_int(permission_to_recorded_employee)
         only_actives = self.__bool_to_int(only_actives)
-        consider_freeze = self.__bool_to_int(consider_freeze)
+        consider_freezed = self.__bool_to_int(consider_freezed)
         only_officials = self.__bool_to_int(only_officials)
         not_temporaries = self.__bool_to_int(not_temporaries)
 
@@ -59,7 +72,7 @@ class LgApiPositionClient(BaseLgServiceClient):
             "QuantidadeDeItensRetornados": records_limit,
             "ComPermissaoParaCadastrarColaborador": permission_to_recorded_employee,
             "SomenteAtivos": only_actives,
-            "ConsiderarPosicoesCongeladas": consider_freeze,
+            "ConsiderarPosicoesCongeladas": consider_freezed,
             "ConsiderarPosicoesApenasDoTipoOficial": only_officials,
             "ConsiderarPosicoesDiferentesDoTipoTemporaria": not_temporaries,
             "TermoDeBusca": search_term,
@@ -82,20 +95,32 @@ class LgApiPositionClient(BaseLgServiceClient):
         search_term: str = None,
         permission_to_recorded_employee: bool = None,
         only_actives: bool = None,
-        consider_freeze: bool = None,
+        consider_freezed: bool = None,
         only_officials: bool = None,
         not_temporaries: bool = None,
     ) -> LgApiPaginationReturn:
+        """Consult list of positions on demand.
+
+        Args:
+            company_code (int, mandatory): The company code
+            permission_to_recorded_employee (bool, optional): Flag to return only positions with permition
+            to recorded employees
+            only_actives (bool, optional): Flag to return only active positions
+            consider_freezed (bool, optional): Flag to consider freezed positions
+            only_officials (bool, optional): Flag to return only official positions
+            not_temporaries (bool, optional): Flag to not return temporaries positions
+            search_term (str, optional): Search term
+        """
         permission_to_recorded_employee = self.__bool_to_int(permission_to_recorded_employee)
         only_actives = self.__bool_to_int(only_actives)
-        consider_freeze = self.__bool_to_int(consider_freeze)
+        consider_freezed = self.__bool_to_int(consider_freezed)
         only_officials = self.__bool_to_int(only_officials)
         not_temporaries = self.__bool_to_int(not_temporaries)
 
         params = {
             "SomenteComPermissaoParaCadastrarColaborador": permission_to_recorded_employee,
             "SomenteAtivos": only_actives,
-            "ConsiderarPosicoesCongeladas": consider_freeze,
+            "ConsiderarPosicoesCongeladas": consider_freezed,
             "ConsiderarPosicoesApenasDoTipoOficial": only_officials,
             "ConsiderarPosicoesDiferentesDoTipoTemporaria": not_temporaries,
             "TermoDaBusca": search_term,
@@ -117,6 +142,11 @@ class LgApiPositionClient(BaseLgServiceClient):
         )
     
     def consult(self, code: int) -> LgApiReturn:
+        """Retrieve a position by code.
+
+        Args:
+            code (int, mandatory): LG position code
+        """
         params = {"Codigo": code}
         return LgApiReturn(
             **serialize_object(
@@ -165,7 +195,7 @@ class LgApiPositionClient(BaseLgServiceClient):
         short_description: str = None,
         long_description: str = None,
         education_level: EnumGrauDeInstrucao = None,
-        edication_notes: str = None,
+        education_notes: str = None,
         experience: EnumMesesExperiencia = None,
         experience_notes: str = None,
         competences_group_list: list[tuple[int, int]] = None,
@@ -177,6 +207,62 @@ class LgApiPositionClient(BaseLgServiceClient):
         original_position_status: EnumTipoStatus = None,
         original_position_description: str = None,
     ) -> LgApiReturn:
+        """Save position on LG system.
+
+        Args:
+            code (int, mandatory): LG position code 
+            description (str, mandatory): Description of the position, like a name
+            status (EnumTipoStatus, mandatory): Status of position, if it is active, inactive and others
+            start_date (date, mandatory): Start date of position. Can be used to set another record on
+                position changes history
+            type (int, mandatory): Type of position code
+            reason_code (int, mandatory): Reason of position code
+            end_date (date, optional): End date of the position
+            jobs_quantity (int, optional): Quantity of jobs available in that position
+            enable_employee_enrollment (bool, optional): Flag to enable or disable employee enrollment at
+                that position
+            is_key (bool, optional): Flag if this is a key position
+            freeze (bool, optional): Flag to freeze or defreeze position
+            freeze_end_date (date, optional): End date of freezing
+            company_code (int, mandatory): Company code of the position
+            office_code (int, optional): Office code of the position
+            work_local_code (str, optional): Work local code
+            tax_jurisdiction_code (int, optional): The tax jurisdiction code
+            cost_center_code (str, optional): Cost center code
+            professional_nature_code (int, optional): Professional nature code
+            responsible_authority_code (int, optional): Responsible Authority code
+            permanent_role_code (int, optional): Position role code
+            commissioned_role_code (int, optional): Position commissioned role code
+            union_code (int, optional): Union code
+            proffessional_liberal_union_code (int, optional): Professional unit code
+            organizational_unit_code (int, optional): Organizational unit code
+            unique_combination (bool, optional): Flag if this position have unique combination. This will prevent
+                that another positions to have the same composition of this one
+            timekeeping_required (bool, optional): Flag if timekeeping is required or not to this position
+            timekeeping_type (EnumTipoPonto, optional): Type of timekeeping
+            work_shift_code (str, optional): Work shift code
+            function_code (int, optional): Function code
+            commissioned_function_code (int, optional): Commissioned function code
+            areas_of_expertise_list (list[int], optional): List of areas of expertise codes
+            hierarchical_level_code (int, optional): Hierarchical level code
+            short_description (str, optional): Short description of this position
+            long_description (str, optional): Long description of this position
+            education_level (EnumGrauDeInstrucao, optional): Education level of this position
+            education_notes (str, optional): Education notes of this position
+            experience (EnumMesesExperiencia, optional): Experience time of this position
+            experience_notes (str, optional): Experience notes of this position
+            competences_group_list (list[tuple[int, int]], optional): Competences group of this position. To
+                define it, you need to pass a list of tuples containing `[(competence_group_code, percent)]`
+            competences_profile_list (list[tuple[int, int, int]], optional): List of competences profiles. To
+                define it, you need to pass a list of tuples containing `[(weight, competence_code, competence_group_code)]`
+            replicate_positions (bool, optional): Flag to create replicated position
+            replication_context (EnumModeloPosicao, optional): Identify if the replica is temporary or dupplicated
+            original_position_transfer_employees (bool, optional): Indicates if the employees in original position
+                have to be transfered to replica position
+            original_position_code (int, optional): Code of original position
+            original_position_status (EnumTipoStatus, optional): Status of original position
+            original_position_description (str, optional): Description of original position
+        """
         date_frmt = "%Y-%m-%d"
         if isinstance(start_date, date):
             start_date = start_date.strftime(date_frmt)
@@ -243,7 +329,7 @@ class LgApiPositionClient(BaseLgServiceClient):
                     "DescricaoResumida": short_description,
                     "DescricaoDetalhada": long_description,
                     "GrauDeInstrucao": education_level,
-                    "ObservacaoEscolaridade": edication_notes,
+                    "ObservacaoEscolaridade": education_notes,
                     "Experiencia": experience,
                     "ObservacaoExperiencia": experience_notes
                 },
@@ -293,9 +379,15 @@ class LgApiPositionClient(BaseLgServiceClient):
     
     def delete(
         self,
-        historical_start_date: date,
-        code: int
+        code: int,
+        historical_start_date: date = None,
     ) -> LgApiExecReturn:
+        """Delete a position
+
+        Args:
+            code (int, mandatory): Code of LG position
+            historical_start_date (date, optional): The start date of historic record
+        """
         params = {
             "DataInicial": historical_start_date.strftime("%Y-%m-%d"),
             "Codigo": code
